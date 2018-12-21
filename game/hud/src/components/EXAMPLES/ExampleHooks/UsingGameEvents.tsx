dd5262bbabdb54ae664cc28cfeabab2ca26afbcf
saddieeiddas <saddie.eiddas@gmail.com>
2018-12-21 21:10:04 +0000

    wip

--- game/hud/src/components/EXAMPLES/ExampleHooks/UsingGameEvents.tsx
+++ game/hud/src/components/EXAMPLES/ExampleHooks/UsingGameEvents.tsx
@@ -4,7 +4,7 @@
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
 
-import React, { useEffect, useState, useCallback } from 'react';
+import React, { useEffect, useState, useCallback, useRef } from 'react';
 
 // this example shows how react hooks can be used to work with events
 // e.g. game.on('foobar', (data) => {});
@@ -21,6 +21,7 @@ const UsingGameEvents = () => {
   const [foobarLog, setFoobarLog] = useState([]);
 
   // example using useEffect to listen to game events.
+  // this example is very verbose, and can be simplified to reduce the amount of code
   useEffect(() => {
     console.log('ExampleHooks-UsingGameEvents', 'registered foobar listener');
     const handle = game.on('foobar', (data: any) => {
@@ -35,7 +36,7 @@ const UsingGameEvents = () => {
     };
   }, [foobarLog]); // ensure this effect is only updated when foobarLog changes
 
-  // the above example can be simplified a bit by returning the clear function directly e.g.
+  // the above example can be simplified by returning the clear function directly e.g.
   useEffect(() => game.on('foobar2', (data: any) => {
     setFoobar(data);
     setFoobarLog([...foobarLog, data]);
@@ -51,20 +52,41 @@ const UsingGameEvents = () => {
     setFoobarField(event.target.value);
   }, []); // ensure this callback is only registered once via empty array here
 
+  // we will also make use of the `useRef` hook to check which button was clicked
+  // to submit the form
+  // create a ref for submit button 1 and 2
+  const submit1: React.MutableRefObject<HTMLButtonElement> = useRef(null);
+  const submit2: React.MutableRefObject<HTMLButtonElement> = useRef(null);
+
   // callback to trigger foobar game event with foobarField value
-  const triggerFoobar = useCallback(() => {
-    game.trigger('foobar', foobarField); // send foobar event
+  const handleFormSubmit = useCallback((event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    let eventName;
+    if (document.activeElement === submit1.current) {
+      console.log('ExampleHooks-UsingGameEvents', 'form submit foobar');
+      eventName = 'foobar';
+    } else if (document.activeElement === submit2.current) {
+      console.log('ExampleHooks-UsingGameEvents', 'form submit foobar2');
+      eventName = 'foobar2';
+    } else {
+      console.log('ExampleHooks-UsingGameEvents', 'form submit foobar (default)');
+      eventName = 'foobar';
+    }
+    game.trigger(eventName, foobarField); // send foobar event
     setFoobarField(''); // clear the foobar field
   }, [foobarField]); // ensure this callback is updated when foobarField changes
 
   return (
     <div>
       <h3>Using Game Events</h3>
-      <h5>Latest 'foobar' event data {foobar}</h5>
-      <input type='text' value={foobarField} onChange={onFoobarFieldChange} onBlur={onFoobarFieldChange} />
-      <button onClick={triggerFoobar}>Send Foobar Event</button>
-      <h4>Log (Scroll)</h4>
-      <ul>
+      <h5>Latest 'foobar' or 'foobar2' event data {foobar}</h5>
+      <form onSubmit={handleFormSubmit}>
+        <input type='text' value={foobarField} onChange={onFoobarFieldChange} onBlur={onFoobarFieldChange} />
+        <button type='submit' name='foobar' ref={submit1}>Send Foobar Event</button>
+        <button type='submit' name='foobar2' ref={submit2}>Send Foobar2 Event</button>
+      </form>
+      <h4>Log (Scroll) {foobarLog.length}</h4>
+      <ul style={{ height: 100, overflow: 'scroll' }}>
         {foobarLog.map((foobarLogItem, index) => (
           <li key={index}>{foobarLogItem}</li>
         ))}
